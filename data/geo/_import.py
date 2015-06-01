e28bc17757421957631a9f2d3a8b917421e2327e
sylvaindurand <sylvaindurand@users.noreply.github.com>
2015-06-01 20:45:56 +0200

    Script fetching departements and cantons

--- data/geo/_import.py
+++ data/geo/_import.py
@@ -1,20 +1,24 @@
-import io, os, csv, requests, tarfile, subprocess, shutil
+import io, os, csv, requests, tarfile, zipfile, subprocess, shutil
 
-source = 'http://export.openstreetmap.fr/contours-administratifs/'
+shp = [["http://osm13.openstreetmap.fr/~cquest/openfla/export/", "departements-20140306-5m", "-shp.zip", "departements"],
+         ["http://osm13.openstreetmap.fr/~cquest/openfla/export/", "cantons-2015",             "-shp.zip", "cantons"]]
 
-files = list(csv.reader(open( '_source.csv' ),skipinitialspace=1))
+def process(shp):
+  for f in shp:
 
-for f in files:
+    print('Processing '+f[1]+'...')
+    url = requests.get(f[0]+f[1]+f[2])
 
-  print('Reading '+f[1]+'...')
-  url = requests.get(source+f[0]+f[1]+f[2]+'.tar.gz')
+    if f[2][-7:] == ".tar.gz":
+      tarfile.open(mode= "r:gz", fileobj = io.BytesIO(url.content)).extractall(path="./shp")
+    elif f[2][-4:] == ".zip":
+      zipfile.ZipFile(io.BytesIO(url.content)).extractall(path="./shp")
 
-  tarfile.open(mode= "r:gz", fileobj = io.BytesIO(url.content)).extractall(path="./shp")
+    subprocess.call(['mapshaper', '-i', './shp/*.shp', '-simplify', 'visvalingam', '1%',
+                     '-o', 'force', 'id-field=insee', 'drop-table', 
+                     'format=topojson', 'topo/'+f[3]+'.json'])
 
-  for i in f[4]:
-    subprocess.call(['mapshaper', '-i', './shp/'+f[1]+'.shp', 
-                      '-simplify', 'visvalingam', i+'%', 
-                      '-o', 'force', 'format=topojson', 'topo/'+f[3]+'-'+i+'.json'],
-                      stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
+    shutil.rmtree('shp')
 
-  shutil.rmtree('shp')
+process(shp)
+
