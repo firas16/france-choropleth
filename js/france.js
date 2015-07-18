dbfb39a78ca6638da983ffd03a8f7db4a64bef7f
sylvaindurand <sylvaindurand@users.noreply.github.com>
2015-07-18 23:47:05 +0200

    Lighter TopoJSON files and new identifiers

--- js/france.js
+++ js/france.js
@@ -1,8 +1,8 @@
 function france(map) {
 
-  $.getJSON("/data/topojson/can.json", function(json) {
+  $.getJSON("/data/topojson/can.topojson", function(json) {
     read(json);
-    $.getJSON("/data/topojson/dep.json", function(json) {
+    $.getJSON("/data/topojson/dep.topojson", function(json) {
       read(json);
       reset();
     });
@@ -18,19 +18,12 @@ function france(map) {
         geojson = topojson.feature(data, data.objects[key]);
         new L.GeoJSON(geojson, {
           onEachFeature: function (feature, json) {
-            switch (feature.properties.insee.length) {
-              case 6: id = "can" + feature.properties.insee.substring(1,3); break;
-              case 5: id = "com" + feature.properties.insee.substring(0,2); break;
-              default: id = "dep";
-            }
-            var el = layers[id];
+            var el = layers[key];
             if (el === undefined) {
               el = new L.layerGroup();
-              layers[id] = el;
+              layers[key] = el;
             }
             el.addLayer(json);
-            json.on('mouseover', function () { info.update(feature.properties.nom) });
-            json.on('mouseout', function () { info.update() });
           },
           style: {
             fillColor: "#ccc",
@@ -46,24 +39,24 @@ function france(map) {
   function reset() {
     if(map.getZoom() <= 8)
       for (el in layers) {
-        if (el.substring(0,3) == "com") map.removeLayer(layers[el]);
-        if (el.substring(0,3) == "can") map.addLayer(layers[el]);
+        if (el.slice(0,3) == "com") map.removeLayer(layers[el]);
+        if (el.slice(0,3) == "can") map.addLayer(layers[el]);
       }
     else
       for (dep in layers["dep"]["_layers"]) {
         d = layers["dep"]["_layers"][dep]; id=d.feature.id;
         if (map.getBounds().contains(d.getBounds()) ||
             map.getBounds().intersects(d.getBounds())) {
-          if (layers["com"+id] != undefined)
-            map.addLayer(layers["com"+id]).removeLayer(layers["can"+id]);
+          if (layers["com-"+id] != undefined)
+            map.addLayer(layers["com-"+id]).removeLayer(layers["can-"+id]);
           else
-            (function(id){ $.getJSON("/data/topojson/com"+id+".json", function(json) {
-              if (layers["can"+id].com != true){
+            (function(id){ $.getJSON("/data/topojson/com"+id+".topojson", function(json) {
+              if (layers["can-"+id].com != true){
                 read(json);
-                layers["can"+id].com = true;
+                layers["can-"+id].com = true;
               }
               if(map.getZoom()>8)
-                map.addLayer(layers["com"+id]).removeLayer(layers["can"+id]);
+                map.addLayer(layers["com-"+id]).removeLayer(layers["can-"+id]);
             });})(id);        
          }
       }
@@ -72,17 +65,4 @@ function france(map) {
     else
       map.removeLayer(layers["dep"]);
   }
-
-  window.info = L.control();
-  info.onAdd = function (map) {
-      this._div = L.DomUtil.create('div', 'info'); 
-      this.update();
-      return this._div;
-  };
-  info.update = function (props) {
-    this._div.innerHTML = '<h4>Carte administrative</h4>' 
-       + (props ? props : '<span style="color:#aaa">Survolez un territoire</span>')
-  };
-  info.addTo(map);
-  
 }
