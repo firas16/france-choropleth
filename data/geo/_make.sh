1a1c757b52f7d1ed7b9b9f61d849eabd4783d31b
sylvaindurand <sylvaindurand@users.noreply.github.com>
2015-07-29 00:38:23 +0200

    Generate cantons from list, with only one command

--- data/geo/_make.sh
+++ data/geo/_make.sh
@@ -17,15 +17,11 @@ mapshaper -i tmp/dep.topojson -simplify visvalingam 1% -o drop-table force id-fi
 # Communes to cantons correspondence
 iconv -f iso-8859-15 -t utf-8 tmp/comsimp2015.txt > tmp/comsimp2015-utf8.txt
 sed 's/	/,/g' tmp/comsimp2015-utf8.txt > tmp/comsimp2015.csv
-awk -F, 'NR > 1 {OFS=","; print $4$5," "$4"-"$7}' tmp/comsimp2015.csv > tmp/comsimp2015-filtered.csv
-sed -e '/-$/d' tmp/comsimp2015-filtered.csv > tmp/communes-cantons.csv
+awk -F, '{OFS=","; print $4$5,$4"-"$7}' tmp/comsimp2015.csv > tmp/cog.txt
+sed '/-$/d;1s/.*/insee,canton/' tmp/cog.txt > tmp/cog.csv
 
 # Generate cantons
-mapshaper -i tmp/cantons_2015.shp -rename-layers can -each 'insee=ref.substring(1,6), obj=ref.substring(1,3), delete ref, delete bureau, delete canton, delete dep, delete jorf, delete population, delete Nom_1, delete wikipedia' -o force id-field=insee tmp/can.topojson
-mapshaper -i tmp/communes-20150101-5m.shp -rename-layers can -each 'obj=insee.substring(0,2), delete surf_m2, delete wikipedia' -filter "['75101','75102','75103','75104','75105','75106','75107','75108','75109','75110','75111','75112','75113','75114','75115','75116','75117','75118','75119','75120','69381','69382','69383','69384','69385','69386','69387','69388','69389','69003','69029','69033','69034','69040','69044','69046','69271','69063','69273','69068','69069','69071','69072','69275','69081','69276','69085','69087','69088','69089','69278','69091','69096','69100','69279','69116','69117','69127','69282','69283','69284','69142','69143','69149','69152','69153','69163','69286','69168','69191','69194','69202','69199','69204','69205','69207','69290','69233','69292','69293','69296','69244','69250','69256','69259','69260','69266'].indexOf(insee)>=0" -o force id-field=insee tmp/metropoles.topojson
-mapshaper -i tmp/metropoles.topojson tmp/can.topojson combine-files -merge-layers -simplify visvalingam 1% -o force id-field=insee tmp/can.topojson
-mapshaper -i tmp/can.topojson -split obj -o force id-field=insee tmp/can.topojson
-mapshaper -i tmp/can.topojson -o drop-table force id-field=insee can.topojson
+mapshaper tmp/communes-20150101-5m.shp -join tmp/cog.csv keys=insee,insee:str -each 'insee = canton || insee, obj = insee.slice(0,2)' -rename-layers can -split obj -dissolve insee -simplify visvalingam 1% -o drop-table force id-field=insee can.topojson
 
 # Generate communes
 mapshaper -i tmp/communes-20150101-5m.shp -simplify visvalingam 10% -o force id-field=insee tmp/communes.topojson ; \
