e9fac0b7c53489d083e62000867fb3633540bc4b
sylvaindurand <sylvaindurand@users.noreply.github.com>
2015-08-02 14:46:30 +0200

    Use object notation for internal functions

--- france.js
+++ france.js
@@ -9,71 +9,71 @@ function france(map, url, domain, range, name, unit) {
     map.on({zoomend: draw, dragend: draw});
   })})});
 
-  function load(json) {
-    for (key in json.objects) {
-      geojson = topojson.feature(json, json.objects[key]);
-      new L.GeoJSON(geojson, { smoothFactor: 0,
-        onEachFeature: function (feature, json) {
-          layers[key] = layers[key] || new L.layerGroup();
-          layers[key].addLayer(json);
-          json.on({
-            mouseover: function(e) { e.target.setStyle({stroke: 1}); info.update(e.target.feature.id); },
-             mouseout: function(e) { e.target.setStyle({stroke: 0}); info.update(); }
-          });
-        },
-        style: function(feature){
-          return { color: "#333", weight: 1, stroke: 0, opacity: .5,
-             fillOpacity: d3.scale.log().clamp(1).domain([1,15000]).range([0,1])(densities[feature.id]),
-               fillColor: d3.scale.linear().clamp(1).domain(domain).range(range)(stat[feature.id])
-          }
-        }
-      })
-    }
-  }
+  this.load = function(json) {
+                for (key in json.objects) {
+                  geojson = topojson.feature(json, json.objects[key]);
+                  new L.GeoJSON(geojson, { smoothFactor: 0,
+                    onEachFeature: function (feature, json) {
+                      layers[key] = layers[key] || new L.layerGroup();
+                      layers[key].addLayer(json);
+                      json.on({
+                        mouseover: function(e) { e.target.setStyle({stroke: 1}); info.update(e.target.feature.id); },
+                         mouseout: function(e) { e.target.setStyle({stroke: 0}); info.update(); }
+                      });
+                    },
+                    style: function(feature){
+                      return { color: "#333", weight: 1, stroke: 0, opacity: .5,
+                         fillOpacity: d3.scale.log().clamp(1).domain([1,15000]).range([0,1])(densities[feature.id]),
+                           fillColor: d3.scale.linear().clamp(1).domain(domain).range(range)(stat[feature.id])
+                      }
+                    }
+                  })
+                }
+              }
 
-  function draw() {
-    if(map.getZoom() <= 8) {
-      for (l in layers) {
-        if (l.slice(0,3) == "com") map.removeLayer(layers[l]);
-        if (l.slice(0,3) == "can") map.addLayer(layers[l]);
-      }
-    }
-    else {
-      for (dep in d=layers["dep"]["_layers"]) {
-        if (map.getBounds().overlaps(d[dep].getBounds())) {
-          function reload(i) { map.addLayer(layers["com-"+i]).removeLayer(layers["can-"+i]); }
-          (function(i) { if (layers["com-"+i]) reload(i);
-            else d3.json('/data/geo/com'+i+'.topojson', function (e, json){ load(json); reload(i); })
-          })(d[dep].feature.id)
-        }
-      }
-    }
-  }
+  this.draw = function() {
+                if(map.getZoom() <= 8) {
+                  for (l in layers) {
+                    if (l.slice(0,3) == "com") map.removeLayer(layers[l]);
+                    if (l.slice(0,3) == "can") map.addLayer(layers[l]);
+                  }
+                }
+                else {
+                  for (dep in d=layers["dep"]["_layers"]) {
+                    if (map.getBounds().overlaps(d[dep].getBounds())) {
+                      function reload(i) { map.addLayer(layers["com-"+i]).removeLayer(layers["can-"+i]); }
+                      (function(i) { if (layers["com-"+i]) reload(i);
+                        else d3.json('/data/geo/com'+i+'.topojson', function (e, json){ load(json); reload(i); })
+                      })(d[dep].feature.id)
+                    }
+                  }
+                }
+              }
 
-  function read(csv, col=1) {
-    array = {};
-    for (obj in csv) array[csv[obj].insee] = csv[obj][Object.keys(csv[0])[col]];
-    return array;
-  }
+  this.read = function(csv, col=1) {
+                array = {};
+                for (obj in csv) array[csv[obj].insee] = csv[obj][Object.keys(csv[0])[col]];
+                return array;
+              }
 
-  function init(data, stats) {
-    layers = {};
-    names = read(data);
-    densities = read(data,2);
-    stat = read(stats);
-    info = new (L.Control.extend({
-      onAdd: function () {
-        div = L.DomUtil.create('div', 'info');
-        this.update();
-        return div;
-      },
-      update: function (props) {
-        div.innerHTML = '<h4>'+name+'</h4>'
-        + (props ? names[props].replace("e Arr", "<sup>ème</sup> arr")+" : "+stat[props].replace(".", ",")+"&nbsp;"+unit
-          : '<span style="color:#aaa">Survolez un territoire</span>')
-      }
-    }));
-    info.addTo(map);
-  }
+  this.init = function(data, stats) {
+                layers = {};
+                names = read(data);
+                densities = read(data,2);
+                stat = read(stats);
+                info = new (L.Control.extend({
+                  onAdd: function () {
+                    div = L.DomUtil.create('div', 'info');
+                    this.update();
+                    return div;
+                  },
+                  update: function (props) {
+                    div.innerHTML = '<h4>'+name+'</h4>'
+                    + (props ? names[props].replace("e Arr", "<sup>ème</sup> arr")+" : "+stat[props].replace(".", ",")+"&nbsp;"+unit
+                      : '<span style="color:#aaa">Survolez un territoire</span>')
+                  }
+                }));
+                info.addTo(map);
+              }
 
 }
